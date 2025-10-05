58ad3b81c7c64bd07c236b78d3d98ee612b4ff6a
anghoo <leeanghoo2@gmail.com>
2025-10-05 23:49:00 +0900

    feat: add read and create API test code

---
+++ tests/test_main.py
@@ -0,0 +1,62 @@
+from fastapi.testclient import TestClient
+
+from app.schemas.product import ProductCreate
+
+
+def test_get_all_count_products(db_client: TestClient):
+    product1 = ProductCreate(
+        name="test1",
+        price=100,
+        quantity=10,
+        description="test1",
+    )
+    product2 = ProductCreate(
+        name="test2",
+        price=100,
+        quantity=10,
+        description="test2",
+    )
+
+    db_client.post(
+        "/products",
+        json=product1.model_dump(),
+    )
+    db_client.post(
+        "/products",
+        json=product2.model_dump(),
+    )
+
+    response = db_client.get("/products")
+    assert response.status_code == 200
+    assert response.json()["product count"] == 2
+
+
+def test_create_product(db_client: TestClient):
+    test_product = ProductCreate(
+        name="Test Product",
+        price=100,
+        quantity=10,
+        description="Test Product Description",
+    )
+
+    response = db_client.post("/products", json=test_product.model_dump())
+    assert response.status_code == 201
+
+    response_data = response.json()
+    assert response_data["name"] == test_product.name
+    assert response_data["price"] == test_product.price
+    assert response_data["quantity"] == test_product.quantity
+    assert response_data["description"] == test_product.description
+    assert "product_id" in response_data
+
+
+def test_get_product_by_id():
+    assert False
+
+
+def test_update_product():
+    assert False
+
+
+def test_delete_product_by_id():
+    assert False
